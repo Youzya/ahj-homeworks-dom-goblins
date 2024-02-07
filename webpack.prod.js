10aedb5de2d0d794031636646501edc72e95d272
aleshaeslichto <alfjkee@gmail.com>
2024-02-07 17:54:07 +0300

    minimizer reset

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,9 +4,5 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  optimization: {
-    minimizer: [
-      new CssMinimizerPlugin(),
-    ],
-  },
+
 });
